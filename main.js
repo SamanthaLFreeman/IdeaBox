1cb61a79a4a1883d5ac72310eb6d9515584e5713
Julian Enochs <julianenochs@yahoo.com>
2019-05-21 14:44:54 -0600

    Add JS global variables and event listeners

---
+++ main.js
@@ -0,0 +1,16 @@
+var starredIdeasBtn = document.querySelector('#js-show-starred-ideas-btn');
+var swillQuality = document.querySelector('#js-swill');
+var plausibleQuality = document.querySelector('#js-plausible');
+var geniusQuality = document.querySelector('#js-genius');
+var newQualityInput = document.querySelector('#js-new-quality-input');
+var newQualityBtn = document.querySelector('#js-new-quality-btn');
+var titleInput = document.querySelector('#js-title-input');
+var bodyInput = document.querySelector('#js-body-input');
+var saveBtn = document.querySelector('#js-save-btn');
+var searchBtn = document.querySelector('#js-search-btn')
+var searchInput = document.querySelector('#js-search-input');
+
+starredIdeasBtn.addEventListener('click', function());
+newQualityBtn.addEventListener('click', function());
+saveBtn.addEventListener('click', function());
+searchBtn.addEventListener('click', function());
