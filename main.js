49239af413058165c1ee2c203865d1db170ba913
Alan Turing <freemasl.sf@gmail.com>
2019-05-21 21:32:18 -0600

    Add input and add new card functionality

--- main.js
+++ main.js
@@ -10,7 +10,44 @@ var saveBtn = document.querySelector('#js-save-btn');
 var searchBtn = document.querySelector('#js-search-btn')
 var searchInput = document.querySelector('#js-search-input');
 
-starredIdeasBtn.addEventListener('click', function());
-newQualityBtn.addEventListener('click', function());
-saveBtn.addEventListener('click', function());
-searchBtn.addEventListener('click', function());
+var ideaTitle = document.querySelector('#js-idea-title');
+var ideaBody = document.querySelector('#js-idea-body');
+var bottomSection = document.querySelector('#js-bottom-section')
+
+// starredIdeasBtn.addEventListener('click', function());
+// newQualityBtn.addEventListener('click', function());
+saveBtn.addEventListener('click', updateIdeaText);
+// searchBtn.addEventListener('click', function());
+
+//When the user clicks save button, the text in the ideas are updated
+//the function listens for a click and revises content
+function updateIdeaText() {
+  createNewCard();
+  ideaTitle.innerText = titleInput.value;
+  ideaBody.innerText = bodyInput.value;
+  
+}
+
+//When save is clicked a new card appears in the bottom section
+function createNewCard() {
+  bottomSection.insertAdjacentHTML('afterbegin',
+    `<article>
+      <div class='top-card'> 
+        <img src='Images/star.svg' alt='star-icon' class='star' id='js-star' />
+        <img src='Images/delete.svg' alt='delete-icon' class='delete' id='js-delete' />
+      </div>
+      <div class='body-card'> 
+        <h3 id='js-idea-title'>
+          ${titleInput.value}
+        </h3>
+        <p id='js-idea-body'>
+          ${bodyInput.value}
+        </p>
+      </div>
+      <div class='bottom-card'> 
+        <img src='Images/upvote.svg' alt='up-arrow-icon' id='js-up-arrow-svg' />
+        <p class='quality-text'> Quality: <span class='quality-value' id='js-quality-value'>Swill</span></p>
+        <img src='Images/downvote.svg' alt='down-arrow-icon' id='js-down-arrow-svg' />
+      </div>
+    </article>`
+)};
