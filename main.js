e0ff7f30e411b040afe02ce088c4b9b3727ebb32
Julian Enochs <julianenochs@yahoo.com>
2019-05-22 16:07:45 -0600

    Disable save button

--- main.js
+++ main.js
@@ -14,28 +14,55 @@ var titleText = '';
 var bodyText = '';
 saveBtn.disabled = true;
 
-// showStarredIdeasBtn.addEventListener('click', null);
-// addNewQualityBtn.addEventListener('click', null);
-saveBtn.addEventListener('click', saveIdea);
+//added variables
+var ideaTitle = document.querySelector('#js-idea-title');
+var ideaBody = document.querySelector('#js-idea-body');
+var bottomSection = document.querySelector('#js-bottom-section')
+
+saveBtn.addEventListener('click', createNewCard);
+// starredIdeasBtn.addEventListener('click', null);
+// newQualityBtn.addEventListener('click', null);
 // searchBtn.addEventListener('click', null);
+// saveBtn.addEventListener('click', saveIdea);
 
-titleInput.addEventListener('keyup', function() {
-	saveBtn.disabled = false;
- 	titleText = titleInput.value
-});
+titleInput.addEventListener('keyup', disableBtns);
+bodyInput.addEventListener('keyup', disableBtns);
+//When save is clicked a new card appears in the bottom section
 
-bodyInput.addEventListener('keyup', function(){
-	saveBtn.disabled = false;
-	bodyText = bodyInput.value;
-})
+function createNewCard() {
+  bottomSection.insertAdjacentHTML('afterbegin',
+    `<article>
+      <div class='top-card'> 
+        <img src='Images/star.svg' alt='star-icon' class='star' id='js-star' />
+        <img src='Images/delete.svg' alt='delete-icon' class='delete' id='js-delete' />
+      </div>
+      <div class='body-card'> 
+        <h3 id='js-idea-title'>
+          ${titleInput.value}
+        </h3>
+        <p id='js-idea-body'>
+          ${bodyInput.value}
+        </p>
+      </div>
+      <div class='bottom-card'> 
+        <img src='Images/upvote.svg' alt='up-arrow-icon' id='js-up-arrow-svg' />
+        <p class='quality-text'> Quality: <span class='quality-value' id='js-quality-value'>Swill</span></p>
+        <img src='Images/downvote.svg' alt='down-arrow-icon' id='js-down-arrow-svg' />
+      </div>
+    </article>`
+)
+    clearInputs();
+    saveBtn.disabled = true;
+};
 
-function saveIdea() {
-event.preventDefault();
-var bodyText = bodyInput.value;
-	ideaCard.insertAdjacentHTML('afterbegin', `<h3>
-	  ${titleText}
-	  </h3>
-	  <p>
-	  ${bodyText} 
-	  </p>`)
+//Clears the two input fields (add to new card function, so it clears after a card is created)
+function clearInputs() {
+  titleInput.value = '';
+  bodyInput.value = '';
+}
+
+function disableBtns() {
+	var disabledBtn = titleInput.value === '' || bodyInput.value === ''
+	saveBtn.disabled = disabledBtn;
+	console.log(disabledBtn)
 };
