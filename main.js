bf11333a99906d5abcae7309d60af2e4a147d53c
Julian Enochs <julianenochs@yahoo.com>
2019-05-22 21:42:36 -0600

    Recover toggle button

--- main.js
+++ main.js
@@ -11,21 +11,22 @@ var saveBtn = document.querySelector('#js-save-btn');
 var searchBtn = document.querySelector('#js-search-btn')
 var searchInput = document.querySelector('#js-search-input');
 var ideaCard = document.querySelector('#js-idea-card');
-var ideaTitle = document.querySelector('#js-idea-title');
-var ideaBody = document.querySelector('#js-idea-body');
-var bottomSection = document.querySelector('#js-bottom-section');
 var titleText = '';
 var bodyText = '';
 saveBtn.disabled = true;
+var ideaTitle = document.querySelector('#js-idea-title');
+var ideaBody = document.querySelector('#js-idea-body');
+var bottomSection = document.querySelector('#js-bottom-section');
 
 //Event Listeners
-saveBtn.addEventListener('click', createNewCard);
 // starredIdeasBtn.addEventListener('click', null);
 // newQualityBtn.addEventListener('click', null);
 // searchBtn.addEventListener('click', null);
-// saveBtn.addEventListener('click', saveIdea);
+
+saveBtn.addEventListener('click', createNewCard);
 titleInput.addEventListener('keyup', disableBtns);
 bodyInput.addEventListener('keyup', disableBtns);
+// ideaTitle.addEventListener('click', editIdeaCard);
 
 //When save is clicked a new card appears in the bottom section
 function createNewCard() {
@@ -55,25 +56,45 @@ function createNewCard() {
     saveBtn.disabled = true;
 };
 
-
-function toggleFavorite() {
-	console.log('favoriting')
-	var starButton = document.getElementById('favoriteBtn');
-	starButton.classList.toggle('orangeFavorite')
-}
-
 //Clears the two input fields (add to new card function, so it clears after a card is created)
 function clearInputs() {
   titleInput.value = '';
   bodyInput.value = '';
-}
+};
 
-//Listens for a key up in the title and body inputs, then enables the save button 
+//Lists for a key up in the title and body inputs, then enables the save button 
 function disableBtns() {
+  console.log(disabledBtn);
   var disabledBtn = titleInput.value === '' || bodyInput.value === ''
   saveBtn.disabled = disabledBtn;
-  console.log(disabledBtn)};
+};
+
+function toggleFavorite() {
+  var starButton = document.getElementById('favoriteBtn');
+  starButton.classList.toggle('orangeStar');
+};
+
+// function editIdeaCard() {
+//   ideaTitle.createElement('textarea')
+// }
+
+// ideaTitle.addEventListener('click', editIdeaCard);
+
+
+
+// function editIdeaCard() {
+//   ideaTitle.createElement('textarea')
+// };
+
+//function to remove the card from the screen
+//It listens for a click on the delete button and then clears the card from the page
+
 
 //When a user clicks the title or body of an idea in the list, that text should become an editable text field, pre-populated with the existing idea title or body.
+
+
 //The user should be able to 'commit' their changes by pressing 'enter/return' and by clicking outside the text field
+
+
+//function that changes the 'star' button when it is active or not active
 
